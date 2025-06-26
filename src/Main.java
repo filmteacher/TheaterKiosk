a7d83ba38f0262d16f233e56556d412544c4d1c1
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-06-26 19:15:04 -0400

    Final Project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 0; // num age = 0
+        String trash = ""; // use for bad input which will read as a String
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your age: "); // output "Enter your age: "
+
+        if(in.hasNextInt()) // OK safe to read in an integer
+        {
+            age = in.nextInt();  // input age
+            in.nextLine(); // clears the newline from the buffer
+
+            if (age >=21) // if age >= 21 then
+            {
+                System.out.println("Your age is: "+age+". You are 21 or older. You get a wrist band."); // output "You are 21 or older. You get a wrist band."
+            }
+            else // else
+            {
+                System.out.println("Your age is: "+age+". You are not old enough to enter."); // output "You aren't old enough to enter."
+            } // end if
+        }
+        else // Not an int can't use nextInt() read as String with nextLine() instead
+        {
+            trash = in.nextLine(); // Ok have to read the input as a String
+            System.out.println("You said your age was: " + trash);
+            System.out.println("Run the program again and enter a valid age.");
         }
     }
 }
